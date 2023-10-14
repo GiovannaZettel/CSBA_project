46eda41bcf7b699f4096cefcfa8473018cb1a150
Giovanna <134565259+GiovannaZettel@users.noreply.github.com>
2023-10-14 00:16:34 -0300

    Alterações para utilização de dicionário e layout

--- app/classes.py
+++ app/classes.py
@@ -1,142 +1,136 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.spinner import Spinner
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.checkbox import CheckBox
-from kivy.uix.gridlayout import GridLayout
 
-class PedidoApp(App):
-    def build(self):
-        self.layout = BoxLayout(orientation='vertical', padding=10)
+class PedidoApp(BoxLayout):
+    def tipo_pagamento(self, tipo_p):
+        if tipo_p == 'À vista (espécie)':
+            self.ids.mensagem.text = "Pagamento a vista"
+        elif tipo_p == 'À vista (pix)':
+            self.ids.mensagem.text = "Pagamento por pix"
+        elif tipo_p == 'À vista (cartão de crédito)':
+            self.ids.mensagem.text = "Pagamento por cartão de crédito"
+        elif tipo_p == 'Parcelado 2x no cartão de crédito':
+            self.ids.mensagem.text = "Pagamento parcelado 2x no cartão de crédito"
         
-        # NOME PIZZARIA
-        self.nome_estabelecimento = Label(text='Pizza do Papa')
-        self.layout.add_widget(self.nome_estabelecimento)
-        
-        # NOME CCLIENTE
-        self.nome_usuario = TextInput(hint_text='Digite seu nome', multiline=False)
-        self.layout.add_widget(self.nome_usuario)
-        
-        # SABOR PIZZA
-        self.opcoes_sabores = Spinner(text='Escolha um sabor',
-                                              values=('Quatro queijos', 'Mussarela', 'Portuguesa', 'Calabresa', 'Frango catupiry', 'Crocante'))
-        self.layout.add_widget(self.opcoes_sabores)
-        self.pizzas_tradicionais    = ['Quatro queijos', 'Mussarela', 'Calabresa']
-        self.pizzas_especiais       = ['Portuguesa', 'Frango catupiry', 'Crocante']
+    def sair(self, instance):
+        # Função para sair da aplicação
+        App.get_running_app().stop()
 
-        # TAMANHO PIZZA
-        self.opcoes_tamanhos = Spinner(text='Escolha o tamanho',
-                                      values=('Broto', 'Média', 'Grande', 'Gigante'))
-        self.layout.add_widget(self.opcoes_tamanhos)
-        self.pizzas_tamanhos = ['Broto', 'Média', 'Grande', 'Gigante']
-        
-        # QUANTIDADE PIZZAS
-        self.quantidade_label = Label(text='Digite a quantidade')
-        self.layout.add_widget(self.quantidade_label)
-        self.quantidade = TextInput(hint_text='Quantidade', multiline=False, input_type='number')
-        self.layout.add_widget(self.quantidade)
-        
-        # GRID DE PAGAMENTOS
-        self.pagamento_layout = GridLayout(cols=4, padding=12)
-        
-        self.pagamento_a_vista_esp = CheckBox(group='pagamento', active=False)
-        self.pagamento_label_esp = Label(text='À vista (espécie)')
-        self.pagamento_layout.add_widget(self.pagamento_a_vista_esp)
-        self.pagamento_layout.add_widget(self.pagamento_label_esp)
-        
-        self.pagamento_a_vista_pix = CheckBox(group='pagamento', active=False)
-        self.pagamento_label_pix = Label(text='À vista (pix)')
-        self.pagamento_layout.add_widget(self.pagamento_a_vista_pix)
-        self.pagamento_layout.add_widget(self.pagamento_label_pix)
-        
-        self.pagamento_a_vista_cartao = CheckBox(group='pagamento', active=False)
-        self.pagamento_label_cartao = Label(text='À vista (cartão de crédito)')
-        self.pagamento_layout.add_widget(self.pagamento_a_vista_cartao)
-        self.pagamento_layout.add_widget(self.pagamento_label_cartao)
-        
-        self.pagamento_parcelado = CheckBox(group='pagamento', active=False)
-        self.pagamento_label_parcelado = Label(text='Parcelado 2x cartão de crédito')
-        self.pagamento_layout.add_widget(self.pagamento_parcelado)
-        self.pagamento_layout.add_widget(self.pagamento_label_parcelado)
-
-        self.layout.add_widget(self.pagamento_layout)
-        
-        # CALCULAR CONTA
-        self.calcular_button = Button(text='Calcular', size_hint_y=None, height=40)
-        self.calcular_button.bind(on_press=self.calcular_total)
-        self.layout.add_widget(self.calcular_button)
-        
-        self.resultado = Label(text='', halign='center')
-        self.layout.add_widget(self.resultado)
-        
-        # LIMPAR CAMPOS
-        self.reset_button = Button(text='Limpar Campos', size_hint_y=None, height=40)
-        self.reset_button.bind(on_press=self.limpar_campos)
-        self.layout.add_widget(self.reset_button)
-        
-        # SAIR APLICAÇÃO
-        self.sair_button = Button(text='Sair', size_hint_y=None, height=40)
-        self.sair_button.bind(on_press=self.stop)
-        self.layout.add_widget(self.sair_button)
-        
-        return self.layout
+        #Dicionário para associar funções aos botões
+    funcoes_botoes = {
+        'sair': sair
+    }
     
-    def calcular_total(self, instance):
+    def limpar_campos(self):
+        self.ids.nome_usuario.text = ''
+        self.ids.q1.text = ''
+        self.ids.q2.text = ''
+        self.ids.q3.text = ''
+        self.ids.q4.text = ''
+        self.ids.q5.text = ''
+        self.ids.q6.text = ''
+        self.ids.sp1.text = 'Escolha o tipo de pagamento'
+        self.ids.tmnho.text = 'Esolha o tamanho'
+        self.ids.q_queijo.active = False
+        self.ids.Mus.active = False
+        self.ids.clabrsa.active = False
+        self.ids.portu.active = False
+        self.ids.cat.active = False
+        self.ids.cro.active = False
+        self.ids.mensagem.text=''
+
+    def calcular_total(self):
         valor_total = 0
 
-        try:
-            quantidade = int(self.quantidade.text)
-        except ValueError:
-            self.resultado.text = 'Quantidade inválida, tente novamente!'
-            return
+        sabores_quantidades = {
+            'Quatro queijos': self.ids.q1.text,
+            'Mussarela': self.ids.q2.text,
+            'Calabresa': self.ids.q3.text,
+            'Portuguesa': self.ids.q4.text,
+            'Frango c/ Catupiry': self.ids.q5.text,
+            'Crocante': self.ids.q6.text
+        }
 
-        tamanho_selecionado = self.opcoes_tamanhos.text
-        sabor_selecionado = self.opcoes_sabores.text
+        valores_sabores = {
+            'Quatro queijos': 25,
+            'Mussarela': 20,
+            'Calabresa': 22,
+            'Portuguesa': 23,
+            'Frango c/ Catupiry': 24,
+            'Crocante': 30
+        }
 
-        if tamanho_selecionado == self.pizzas_tamanhos[0]:
-            for j in self.pizzas_tradicionais:
-                if j == sabor_selecionado:
-                    valor_total += 1990
-                    break
-                else:
-                    valor_total += 2490
-                    break
-        elif tamanho_selecionado == self.pizzas_tamanhos[1]:
-            for j in self.pizzas_tradicionais:
-                if j == sabor_selecionado:
-                    valor_total += 2990
-                    break
-                else:
-                    valor_total += 3490
-                    break
-        elif tamanho_selecionado == self.pizzas_tamanhos[2]:
-            for j in self.pizzas_tradicionais:
-                if j == sabor_selecionado:
-                    valor_total += 3990
-                    break
-                else:
-                    valor_total += 4490
-                    break
-        elif tamanho_selecionado == self.pizzas_tamanhos[3]:
-            for j in self.pizzas_tradicionais:
-                if j == sabor_selecionado:
-                    valor_total += 4990
-                    break
-                else:
-                    valor_total += 5490
-                    break
+        for sabor, quantidade_str in sabores_quantidades.items():
+            # Se a quantidade for uma string vazia, consideramos como 0
+            if not quantidade_str:
+                quantidade = 0
+            else:
+                try:
+                    quantidade = int(quantidade_str)
+                except ValueError:
+                    self.ids.mensagem.text = f'Quantidade inválida para {sabor}, tente novamente!'
+                    return
 
-        nome_usuario = self.nome_usuario.text
-        self.resultado.text = f'O valor da compra de {nome_usuario} totalizou R$ {(valor_total * quantidade)/100:.2f}'
+                # Adiciona o valor do sabor ao valor total
+                valor_total += quantidade * valores_sabores[sabor]
+
+        tamanho_selecionado = self.ids.tmnho.text
+
+        # Chame o método para calcular o valor da pizza com base nos sabores selecionados
+        valor_pizza = self.calcular_valor_pizza()
+
+        # Adicione o valor do tamanho da pizza ao valor total
+        if tamanho_selecionado == 'Pequena':
+            valor_total *= 1.0  # Multiplicação pelo tamanho
+        elif tamanho_selecionado == 'Média':
+            valor_total *= 1.2
+        elif tamanho_selecionado == 'Grande':
+            valor_total *= 1.5
+        elif tamanho_selecionado == 'Gigante':
+            valor_total *= 2.0
+
+        # Multiplique pelo número de pizzas
+        quantidade_total = sum(1 for quantidade_str in sabores_quantidades.values() if quantidade_str.strip())
+        valor_total *= quantidade_total
+
+        nome_usuario = self.ids.nome_usuario.text
+        self.ids.mensagem.text = f'O valor da compra de {nome_usuario} totalizou R$ {valor_total:.2f}'
+
+    def calcular_valor_pizza(self):
+        valor_pizza = 0
+
+        # Valores fictícios para cada sabor
+        valores_sabores = {
+            'Quatro queijos': 25,
+            'Mussarela': 20,
+            'Calabresa': 22,
+            'Portuguesa': 23,
+            'Frango c/ Catupiry': 24,
+            'Crocante': 30
+        }
+
+        # Verifique quais checkboxes estão marcados e some os valores dos sabores selecionados
+        if self.ids.q_queijo.active:
+            valor_pizza += valores_sabores['Quatro queijos']
+        if self.ids.Mus.active:
+            valor_pizza += valores_sabores['Mussarela']
+        if self.ids.clabrsa.active:
+            valor_pizza += valores_sabores['Calabresa']
+        if self.ids.portu.active:
+            valor_pizza += valores_sabores['Portuguesa']
+        if self.ids.cat.active:
+            valor_pizza += valores_sabores['Frango c/ Catupiry']
+        if self.ids.cro.active:
+            valor_pizza += valores_sabores['Crocante']
+
+        return valor_pizza
+
+
+                
+class MainApp(App):
+    def build(self):
+        
+        return PedidoApp()
     
-    def limpar_campos(self, instance):
-        self.nome_usuario.text = ''
-        self.opcoes_sabores.text = 'Escolha um sabor'
-        self.quantidade.text = ''
-        self.tipo_pagamento.text = 'Escolha o tipo de pagamento'
-        self.resultado.text = ''
-
 if __name__ == '__main__':
-    PedidoApp().run()
+    MainApp().run()
